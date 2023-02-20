2cdb2cd1d28643c5662be2d350266bb1512a7ceb
Matthew Kirby <matthewkirby@email.arizona.edu>
2023-02-20 11:14:06 -0600

    Disable the download log button and show a warning message that the log is unavailable if no spoiler log is generated.

--- app/components/Result.jsx
+++ app/components/Result.jsx
@@ -15,11 +15,23 @@ const appendHash = (str, hash, ext) => {
   return `${str}.${ext}`;
 };
 
+const generateSpoilerLogWarning = (log) => {
+  if (log === null) {
+    return (<div className='generator-warning'>
+      WARNING: This seed was not generated with a spoiler log. 
+      You will not be able to obtain one later. 
+      If this is a mistake, please generate a new seed.
+    </div>);
+  }
+  return '';
+}
+
 export const Result = ({ rom, log, hash }) => {
   return (
     <div>
       <button onClick={() => download(rom, appendHash('OoTMM', hash, 'z64'), 'application/octet-stream')}>Save ROM</button>
-      <button onClick={() => download(log, appendHash('spoiler', hash, 'txt'), 'text/plain')}>Save Spoiler Log</button>
+      <button disabled={log === null} onClick={() => download(log, appendHash('spoiler', hash, 'txt'), 'text/plain')}>Save Spoiler Log</button>
+      {generateSpoilerLogWarning(log)}
     </div>
   );
 };
